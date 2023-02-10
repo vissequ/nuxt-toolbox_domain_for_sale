98798ecb4577be780f539530052461ab7a771f8b
vissequ <93226346+vissequ@users.noreply.github.com>
2023-02-10 13:42:39 -0800

    Update success.vue

--- pages/success.vue
+++ pages/success.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <h1>Thanks for your feedback!</h1>
+    <h1>Thanks for your offer!</h1>
+    <p>If you use Discord, you can also contact the domain owner at: vissequ#1301</p>
     <NuxtLink to="/">Home</NuxtLink>
   </div>
 </template>
