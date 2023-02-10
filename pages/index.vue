013bc7a114b5785ad31ea07c1ab1a9e5f9630e61
vissequ <93226346+vissequ@users.noreply.github.com>
2023-02-10 13:41:35 -0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,6 @@
   <main>
     <div class="intro">
       <h1>This Domain is for Sale</h1>
-      <p>
-        Hi 👋! This template gives you a
-        <a href="https://nuxtjs.org/">Nuxt</a> app with the scaffolding for
-        <a href="https://www.netlify.com/products/functions/"
-          >Netlify Functions</a
-        >, <a href="https://www.netlify.com/products/forms/">Forms</a>, and
-        <a href="https://docs.netlify.com/routing/redirects/">Redirects</a>. Our
-        aim was to give you the code you would need to hit the ground running
-        with a few fun features.
-      </p>
     </div>
     <FeedbackForm />
     <JokeBlock />
