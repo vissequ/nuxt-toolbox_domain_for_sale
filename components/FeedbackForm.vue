6b7d2680e4cbbacbf4c41c41807073287fd76c9f
vissequ <93226346+vissequ@users.noreply.github.com>
2023-02-10 13:40:40 -0800

    Update FeedbackForm.vue

--- components/FeedbackForm.vue
+++ components/FeedbackForm.vue
@@ -17,7 +17,7 @@
       <input id="name" type="text" name="name" />
       <label for="email">Email</label>
       <input id="email" type="email" name="email" required />
-      <label for="feedback">What is your feedback?</label>
+      <label for="feedback">What is your offer?</label>
       <textarea id="feedback" wrap="soft" name="feedback" required></textarea>
       <button type="submit">Submit</button>
     </form>
